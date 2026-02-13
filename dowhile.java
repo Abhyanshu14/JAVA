c2f94913e5326da1095a5897afd1f7f7434965e8
Abhyanshu <abhyanshu@Abhyanshus-MacBook-Air.local>
2026-02-13 21:25:31 +0530

    do while loop in java

--- dowhile.java
+++ dowhile.java
@@ -2,8 +2,9 @@ public class dowhile {
     public static void main(String[] args) {
         int i=0;
         do{
-            System.out.println(i);
+            System.out.println(i+1);
             i++;
         }while(i<-1);
+
     }
 }
